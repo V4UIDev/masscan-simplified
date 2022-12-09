b48a75c3007a673f68ad8569f9c85d121dfc047b
kat <k.cheshire@zengenti.com>
2022-12-09 12:10:40 +0000

    try again

--- build/ubuntu/masscan_frontend/frontend/views.py
+++ build/ubuntu/masscan_frontend/frontend/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
+from django.core.validators import validate_ipv4_address
+from django.core.exceptions import ValidationError
 import subprocess
 
 from .forms import RequestForm
@@ -7,12 +9,16 @@ from .forms import RequestForm
 # Create your views here.
 def index(request):
 
-    template = loader.get_template('frontend/index.html')
     if request.method == "POST":
         form = RequestForm(request.POST)
 
         subnet = form.data["subnet"]
         ipaddr = form.data["ipaddr"]
+        try:
+            validate_ipv4_address(ipaddr)
+        except ValidationError:
+            return render(request, "frontend/help/help.html")
+
         ipv4 = f"{ipaddr}/{subnet}"
         rate = form.data["rate"]
         lowerboundport = form.data["lowerboundport"]
